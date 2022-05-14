9079a25b6f4529ea73d7de6fd8b8c781ba25f5ca
yuguoc <Canechen@163.com>
2022-05-14 15:41:41 +0800

    Translation method for FEN

--- python/Manto_Carlo_Searching_Tree.py
+++ python/Manto_Carlo_Searching_Tree.py
@@ -1,18 +1,49 @@
+import os
 import pickle
-from math import sqrt,log
+from functools import reduce
+from math import log, sqrt
+
 import numpy as np
+
 from env import XiangqiEnv
 from modules.vec2d import Vec2d
-from functools import reduce
-import tqdm
+
+uct_b=None
+uct_r=None
+tree_s=None
+path_s=None
+number="0"
+if not os.path.exists("./data"):
+    os.mkdir("./data")
+elif len(os.listdir("./data"))==0:
+    pass
+else:
+    filename=os.listdir("./data")
+    number=""
+    name=[]
+    for k in filename:
+        number=""
+        for i in k:
+            if i in ["0","1","2","3","4","5","6","7","8","9"]:
+                number+=i
+        name.append(int(number))
+    number=str(max(name))
+    print(number)
+    with open("./data/UCT_DATA_red_"+number+".txt","rb+") as fin1:
+        uct_r=pickle.load(fin1)
+    with open("./data/Tree_structure_"+number+".txt","rb+") as fin2:
+        tree_s=pickle.load(fin2)
+    with open("./data/UCT_DATA_black_"+number+".txt","rb+") as fin3:
+        uct_b=pickle.load(fin3)
+
 c=1
 chess=XiangqiEnv()
 e=0.9
 def totuple(states):
-    state=()
+    state1=()
     for i in states:
-        state+=tuple(i)
-    return state
+        state1+=tuple(i)
+    return state1
 initial=totuple(chess.state().copy())
 def isNotEmpty(list):
     num=0
@@ -20,9 +51,17 @@ def isNotEmpty(list):
         if i!=None:
             num+=1
     return num
+def total_length(list):
+    n=0
+    for i in list:
+        n+=len(i)
+    return n
 class tree:
     def __init__(self):
-        self.tree={}
+        if tree_s==None:
+            self.tree={}
+        else:
+            self.tree=tree_s
     def add_leaves(self,parent,son,pieces,moves):
         temp=self.tree[parent]
         temp[son]=[pieces,moves]
@@ -32,8 +71,6 @@ class tree:
     def isExist(self,tar):
         return tar in self.tree
     def leaves(self,tar):
-        if tar not in self.tree:
-            self.tree[tar]={}
         return self.tree[tar]
     def structure(self):
         print(self.tree)
@@ -42,25 +79,32 @@ class tree:
 class UCT(tree):
     def __init__(self):
         super().__init__()
+
         states=chess.state().copy()
         state=totuple(states)
         self.UCT={state:[0,0]}
-        self.tree={state:{}}
+        if tree_s==None:
+            self.tree={state:{}}
     def add_leaves(self,parent,son,pieces,moves):
         temp=self.tree[parent]
         temp[son]=[pieces,moves]
-        self.tree[son]={}
-        self.UCT[son]=[0,1]
+        if son not in self.tree:
+            self.tree[son]={}
+            self.UCT[son]=[0,1]
+
         return self.tree
 
     def calc(self,Q,Ni,N,c):
-        possibility=float(Q)/float(N)+c*sqrt(log(Ni)/N)
+        possibility=float(Q)/float(N)+float(c)*sqrt(log(float(Ni))/float(N))
         return possibility
     def update(self,node,win_or_lose):
-        self.UCT[node]=[self.UCT[node][0]+int(win_or_lose),self.UCT[node][1]+1]
+        if win_or_lose:
+            self.UCT[node]=[self.UCT[node][0]+int(win_or_lose),self.UCT[node][1]+1]
+        else:
+            self.UCT[node]=[self.UCT[node][0]+int(-2),self.UCT[node][1]+1]
         return self.UCT
-    def uct(self,node):
-        self.Ni=self.UCT[node][1]
+    def uct(self,node,last_node):
+        self.Ni=self.UCT[last_node][1]
         return self.calc(self.UCT[node][0],self.Ni,self.UCT[node][1],c)
     def data(self):
         return self.UCT
@@ -68,100 +112,122 @@ class UCT(tree):
 class searching_tree(UCT):
     def __init__(self):
         super().__init__()
-        self.path=[initial]
+        if path_s==None:
+            self.path=[initial+(chess.board.turn,)]
+        else:
+            self.path=path_s.copy()
     def random_move(self):
-        self.actionspace=chess.actionSpace()
+        self.actionspace=chess.actionSpace().copy()
         self.actions=[]
         while self.actions==[]:
             self.pieces=np.random.randint(len(self.actionspace))
-            self.actions=self.actionspace[self.pieces]
+            self.actions=self.actionspace[self.pieces]      #Use pop()
         self.action=self.actions[np.random.randint(len(self.actions))]
 
         return [self.pieces,self.action]
- 
+
+        
+        
+
     def choose_biggest(self,node):
         next=self.leaves(node)
         ucts=[]
-        for k in next.keys():
-            ucts.append(self.uct(k))
+        for k in next:
+            ucts.append(self.uct(k,node))
+        print(ucts)
         choose=list(next.keys())[ucts.index(max(ucts))]
+        #print(ucts.index(max(ucts)))
         pieces=self.leaves(node)[choose][0]
         step=self.leaves(node)[choose][1]
         return [pieces,step]
 
         
-    def backwards(self,win_or_lose):
+    def backwards(self,win_or_lose,colour):
+        #print(len(self.path))
         for i in self.path:
-            self.update(i,win_or_lose)
-        self.path=[initial]
+            if i[-1]==colour:
+                self.update(i[:-1],win_or_lose)
+            else:
+                self.update(i[:-1],not win_or_lose)
+        self.path=[initial+(chess.board.turn,)]
         return self.UCT
-    def exploring_c(self,on_or_off):
+    def exploring_c(self,on_or_off=False):
         global e
-        if on_or_off:
-            return len(self.leaves(totuple(chess.state())))<isNotEmpty(chess.actionSpace()) or float(np.random.randn())>e
+        if on_or_off==True:
+            return len(self.leaves(totuple(chess.state())))<int(total_length(chess.actionSpace())) or float(np.random.randn())>e
+        elif on_or_off=="train":
+            return len(self.leaves(totuple(chess.state())))==0
         else:
-            return len(self.leaves(totuple(chess.state())))<isNotEmpty(chess.actionSpace())
-    def train(self,on_or_off=True):
+            return len(self.leaves(totuple(chess.state())))<int(total_length(chess.actionSpace()))
+    def train(self,on_or_off=False):
         global c
-        self.backup_path=[initial]
+        self.backup_path=[initial+(chess.board.turn,)]
         if self.exploring_c(on_or_off):
             
             self.previous_state=chess.state().copy()
             self.move=self.random_move()
-            kill,state,self.signal=chess.step(self.move)
-            self.path.append(totuple(state))
-            if self.isExist(totuple(state)):
+            kill,self.state,self.signal=chess.step(self.move)
+            self.path.append(totuple(self.state)+(chess.board.turn,))
+            self.paths=self.path.copy()
+            #print(len(self.path))
+            if self.isExist(totuple(self.state)):
                 pass
             else:
-                self.add_leaves(totuple(self.previous_state),totuple(state),self.move[0],self.move[1])
+                self.add_leaves(totuple(self.previous_state),totuple(self.state),self.move[0],self.move[1])
             if self.signal=="win":
-                self.backwards(True)
+                self.backwards(True,chess.board.turn)
                 chess.board.reset()
 
             elif self.signal=="lose":
-                self.backwards(False)
+                self.backwards(False,chess.board.turn)
                 chess.board.reset()
         else:
+            print("Thinking")
             self.previous_state=totuple(chess.state())
-            kill,state,self.signal=chess.step(self.choose_biggest(self.previous_state))
-            self.path.append(totuple(state))
-            self.backup_path=self.path.copy()
+            killed,self.state,self.signal=chess.step(self.choose_biggest(self.previous_state))
+            print(killed)
+            self.path.append(totuple(self.state)+(chess.board.turn,))
+            self.paths=self.path.copy()
             if self.signal=="win":
-                self.backwards(True)
+                self.backwards(True,chess.board.turn)
                 chess.board.reset()
             elif self.signal=="lose":
-                self.backwards(False)
+                self.backwards(False,chess.board.turn)
                 chess.board.reset()
-        return self.tree,self.backup_path,self.signal,state
-    def record(self,tree,path,signal,state):
-        self.tree=tree
-        self.path=path
+        return self.tree,self.paths,self.signal,self.state.copy()
+    def record(self,tree,paths,signal,state):
+        self.tree=tree.copy()
+        self.path=paths.copy()
         self.UCT[totuple(state)]=[0,1]
         if signal=="win":
-            self.backwards(False)
+            self.backwards(False,chess.board.turn)
             chess.board.reset()
         elif signal=="lose":
-            self.backwards(True)
+            self.backwards(True,chess.board.turn)
             chess.board.reset()
 
 
 
-black=searching_tree()
-red=searching_tree()
 if __name__=="__main__":
-    last_epoch=1
-    for turn in tqdm.tqdm(range(100000)):
+    
+    black=searching_tree()
+    red=searching_tree()
+    if not uct_b==None:
+        black.UCT=uct_b
+    if not uct_r==None:
+        red.UCT=uct_r
+    
+    i=int(number)+1
+    for turn in range(1,1000001):
         trees,path,signal,state=red.train()
         black.record(trees,path,signal,state)
         trees,path,signal,state=black.train()
         red.record(trees,path,signal,state)
-       # if turn%500==0:
-           # print(red.UCT[initial],"steps:",turn," ","length:",len(red.UCT)," ","Increase",(len(red.UCT)-last_epoch)," ","Increase percentage:",((len(red.UCT)-last_epoch)/last_epoch)*100,"%","\n")
-           # last_epoch=len(red.UCT)
-    
-    
-    with open("UCT_DATA.txt","wb") as fout:
-        pickle.dump(red.UCT,fout)
-
-    with open("Tree_structure.txt","wb") as fout2:
-        pickle.dump(red.tree,fout2)
+        if turn%10000==0:
+            with open("./data/UCT_DATA_red_"+str(i)+".txt","wb") as fout:
+                pickle.dump(red.UCT,fout)
+            with open("./data/Tree_structure_"+str(i)+".txt","wb") as fout2:
+                pickle.dump(red.tree,fout2)
+            with open("./data/UCT_DATA_black_"+str(i)+".txt","wb") as fout3:
+                pickle.dump(black.UCT,fout3)
+            i+=1
--- python/env.py
+++ python/env.py
@@ -3,15 +3,11 @@ from modules.board import Board
 class XiangqiEnv():
     def __init__(self):
         self.board = Board();
-    
     def actionSpace(self):
         return self.board.actionSpaces();
     
     def state(self):
         return self.board.board
-    
-    def reset(self):
-        return self.board.reset()
     
     def step(self, action):
         signal = False
@@ -22,6 +18,65 @@ class XiangqiEnv():
             signal = "win"
         return killed, self.board.board, signal
 
+    def translate(self):
+        board=self.state().copy()
+        trans=[]
+        final_trans=""
+        for i in range(9):
+            tmp=[]
+            for j in board[i]:
+                tmp.append(j)
+            trans.append(tmp*1)
+        for i in range(10):
+            line=""
+            for j in range(9):
+                if trans[j][9-i] == 0:
+                    prt = "K"
+                elif trans[j][9-i] == 1 or trans[j][9-i] == 2:
+                    prt = "A"
+                elif trans[j][9-i] == 3 or trans[j][9-i] == 4:
+                    prt = "B"
+                elif trans[j][9-i] == 5 or trans[j][9-i] == 6:
+                    prt = "N"
+                elif trans[j][9-i] == 7 or trans[j][9-i] == 8:
+                    prt = "R"
+                elif trans[j][9-i] == 9 or trans[j][9-i] == 10:
+                    prt = "C"
+                elif trans[j][9-i] >= 11 and trans[j][9-i] <= 15:
+                    prt = "P"
+                elif trans[j][9-i] == 16:
+                    prt = "k"
+                elif trans[j][9-i] == 17 or trans[j][9-i] == 18:
+                    prt = "a"
+                elif trans[j][9-i] == 19 or trans[j][9-i] == 20:
+                    prt = "b"
+                elif trans[j][9-i] == 21 or trans[j][9-i] == 22:
+                    prt = "n"
+                elif trans[j][9-i] == 23 or trans[j][9-i] == 24:
+                    prt = "r"
+                elif trans[j][9-i] == 25 or trans[j][9-i] == 26:
+                    prt = "c"
+                elif trans[j][9-i] >= 27 and trans[j][9-i] <= 31:
+                    prt = "p"
+                else:
+                    prt=""
+                line+=prt
+                if prt=="":
+                    if len(line)!=0:
+                        if line[-1] in ["0","1","2","3","4","5","6","7","8","9"]:
+                            line=line[:-1]+str(int(line[-1])+1)
+                        else:
+                            line+=str(1)
+                    else:
+                        line+=str(1)
+            final_trans+=line+"/"
+        final_trans=final_trans[0:-1]
+        if self.board.turn=="red":
+            final_trans=final_trans[0:-1]+" w"+" - - "+str(0)+" "+str(self.board.rounds)
+        elif self.board.turn=="black":
+            final_trans=final_trans[0:-1]+" b"+" - - "+str(0)+" "+str(self.board.rounds)
+        return final_trans
+        
     def __str__(self):
         board = self.board.__str__();
         return board;
