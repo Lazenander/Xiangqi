965632f0a30f21dbe2fc6bae883db767d10f673a
Bobby Yang <18018200000@163.com>
2022-05-23 21:06:05 +0800

    update model

---
+++ model/random.py
@@ -0,0 +1,16 @@
+import random
+
+class Model:
+    def action(state, actionSpace):
+        cnt = 0
+        for action in actionSpace:
+            if action != []:
+                cnt += 1
+        index = random.randint(0, cnt - 1);
+        cnt = 0
+        for i in len(actionSpace):
+            if actionSpace[i] == []:
+                continue
+            if cnt == index:
+                action = actionSpace[i][random.randint(0, len(actionSpace[i]) - 1)]
+                return index, action.x, action.y
